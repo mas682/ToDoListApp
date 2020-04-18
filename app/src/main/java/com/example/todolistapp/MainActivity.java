24dea861885cb309f23915756006bde51f07934a
mas682 <mas682@pitt.edu>
2020-04-17 21:07:12 -0400

    Added info button to all reminders

--- app/src/main/java/com/example/todolistapp/MainActivity.java
+++ app/src/main/java/com/example/todolistapp/MainActivity.java
@@ -21,11 +21,13 @@ import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.android.material.drawable.DrawableUtils;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.w3c.dom.Attr;
 
@@ -44,61 +46,105 @@ public class MainActivity extends AppCompatActivity {
         // Get the activity main constraint layout
         ConstraintLayout parentLayout = (ConstraintLayout) findViewById(R.id.activity_main);
         int numViewElems = parentLayout.getChildCount();
-        // I think it starts at
+        // get the last added divider view's index
         int lastDivider = numViewElems - 1;
+        // get the last added divider view
         View lastDiv = parentLayout.getChildAt(lastDivider);
         // create a new EditText view
         EditText textView = new EditText(MainActivity.this);
+        // create a new RadioButton view
         RadioButton radioButton = new RadioButton(MainActivity.this);
+        // create a new FloatingAction view
+        FloatingActionButton infoButton = new FloatingActionButton(MainActivity.this);
+        // create a new View for the divider
         View divider = new View(MainActivity.this);
-        divider.setId(View.generateViewId());
-        // get a Id for the new view
+        // get a Id for the new text view
         textView.setId(View.generateViewId());
         // get a Id for the new radio view
         radioButton.setId(View.generateViewId());
+        // get a Id for the new info button
+        infoButton.setId(View.generateViewId());
+        // get a Id for the new divider
+        divider.setId(View.generateViewId());
         // set the width and height of the new view
         textView.setLayoutParams(new ViewGroup.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
                 ConstraintLayout.LayoutParams.WRAP_CONTENT));
         // set the type of the text view
         textView.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+        // set the padding for the textview
         textView.setPadding(0, 0, 0, 0);
+        // set the background of the textview
         textView.setBackground(null);
+        // set the text color to white
         textView.setTextColor(Color.WHITE);
+        // set the text size to 18sp
         textView.setTextSize(18);
+        // for testing
         textView.setText("The number: " + lastDiv.getId());
+        // set the radio buttons width and height
         radioButton.setLayoutParams(new ViewGroup.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
-        //parse textColor from string hex code
-        int buttonColor = Color.parseColor("#0078FF");
-        //int buttonColor = Color.parseColor(String.valueOf(ContextCompat.getColor(MainActivity.this, R.color.brightBlue)));
-        //set textcolor to radioButton
-        radioButton.setButtonTintList(ColorStateList.valueOf(buttonColor));
+        // get the value of the color accent
+        int colorAccent = Color.parseColor("#0078FF");
+       // int buttonColor = Color.parseColor(String.valueOf(ContextCompat.getColor(MainActivity.this, R.color.brightBlue)));
+        //set the color of the button
+        radioButton.setButtonTintList(ColorStateList.valueOf(colorAccent));
+        // set the size of the button
         radioButton.setScaleX((float)1.5);
         radioButton.setScaleY((float)1.5);
+        /*
+        android:backgroundTint="@color/colorPrimaryDark"
+        app:backgroundTint="@color/colorPrimaryDark"
+        app:maxImageSize="30dp"
+        app:rippleColor="@color/colorPrimaryDark"
+         */
+        // set the height and width for the info button
+        infoButton.setLayoutParams(new ViewGroup.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        // get the color black
+        int colorPrimaryDark = Color.parseColor("#000000");
+        // set the background tint
+        infoButton.setBackgroundTintList(ColorStateList.valueOf(colorPrimaryDark));
+        // set the button as clickable
+        infoButton.setClickable(true);
+        // set the size to 30dp
+        infoButton.setCustomSize((int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30,
+                getResources().getDisplayMetrics()));
+        // set the image source
+        infoButton.setImageResource(R.drawable.baseline_info_24);
+        // make the icon scale to the size of the button
+        infoButton.setScaleType(ImageView.ScaleType.CENTER);
+        // set the color of the divider
         divider.setBackgroundColor(ContextCompat.getColor(MainActivity.this, R.color.colorGray));
-        divider.setLayoutParams(new ViewGroup.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3,
+        // set the height and width of the divider
+        divider.setLayoutParams(new ViewGroup.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 2,
                 getResources().getDisplayMetrics())));
         // add the view to the activity main layout
         // the numViewElems is the index at which to add the view
         parentLayout.addView(radioButton, numViewElems);
         parentLayout.addView(textView, numViewElems + 1);
-        parentLayout.addView(divider, numViewElems + 2);
+        parentLayout.addView(infoButton, numViewElems + 2);
+        parentLayout.addView(divider, numViewElems + 3);
         // set the constraints on where the new view should go inside the activity main layout
         ConstraintSet set = new ConstraintSet();
         set.clone(parentLayout);
+        // text view constraints
         set.connect(textView.getId(), ConstraintSet.START, radioButton.getId(), ConstraintSet.END, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8,
                 getResources().getDisplayMetrics()));
-        // the part in the right is used to set the value in dp
-        set.connect(textView.getId(), ConstraintSet.END, parentLayout.getId(), ConstraintSet.END, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
+        set.connect(textView.getId(), ConstraintSet.END, infoButton.getId(), ConstraintSet.START, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
                 getResources().getDisplayMetrics()));
         set.connect(textView.getId(), ConstraintSet.TOP, lastDiv.getId(), ConstraintSet.BOTTOM, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
                 getResources().getDisplayMetrics()));
-        // radio button
+        // radio button constraints
         set.connect(radioButton.getId(), ConstraintSet.START, parentLayout.getId(), ConstraintSet.START, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
                 getResources().getDisplayMetrics()));
         set.connect(radioButton.getId(), ConstraintSet.TOP, lastDiv.getId(), ConstraintSet.BOTTOM, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
                 getResources().getDisplayMetrics()));
-        // divider
+        // info button constraints
+        set.connect(infoButton.getId(), ConstraintSet.TOP, lastDiv.getId(), ConstraintSet.BOTTOM, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14,
+                getResources().getDisplayMetrics()));
+        set.connect(infoButton.getId(), ConstraintSet.END, parentLayout.getId(), ConstraintSet.END, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16,
+                getResources().getDisplayMetrics()));
+        // divider constraints
         set.connect(divider.getId(), ConstraintSet.TOP, textView.getId(), ConstraintSet.BOTTOM, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8,
                 getResources().getDisplayMetrics()));
         set.connect(divider.getId(), ConstraintSet.START, radioButton.getId(), ConstraintSet.END, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8,
